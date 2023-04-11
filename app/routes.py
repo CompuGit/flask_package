dee48ac37b084e9e1f84266b4eaef5f01e76a9f7
admin compugit <ad.compugit@gmail.com>
2023-04-11 14:06:17 +0530

    flask package - v1

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import *
 from app.models import *
 
+@app.route('/api_key')
+def api_key():
+      return f'API_KEY = { app.config.get("API_KEY") }'
+
 @app.route('/')
 def index():
 	return render_template('index.html')
